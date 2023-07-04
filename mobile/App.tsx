d3c977cff9a6c40b08f59d6c1e5c7210196f7f41
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-03 22:38:16 -0300

    feat: Instaled NativeBase.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native'
+import { NativeBaseProvider } from 'native-base'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NativeBaseProvider>
       <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </NativeBaseProvider>
+  )
+}
