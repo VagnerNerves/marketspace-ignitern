bc33542d4fc77b7f54fd647abef6bfdba44cfcd4
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-04 18:11:07 -0300

    feat: Add Fonts Karla.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,20 @@
-import { StatusBar, Text } from 'react-native'
+import { StatusBar } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
+
+import {
+  Karla_400Regular,
+  Karla_700Bold,
+  useFonts
+} from '@expo-google-fonts/karla'
+
 import { Loading } from '@components/Loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Karla_400Regular,
+    Karla_700Bold
+  })
+
   return (
     <NativeBaseProvider>
       <StatusBar
@@ -10,6 +22,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+
+      {fontsLoaded ? <Loading /> : <Loading />}
     </NativeBaseProvider>
   )
 }
