b118bd50b71c6a38d637d0265c8034d7f2cbe9cf
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-05 22:19:36 -0300

    feat: Created Input.

---
+++ mobile/src/components/Input.tsx
@@ -0,0 +1,57 @@
+import { TouchableOpacity } from 'react-native'
+import {
+  Input as InputNativeBase,
+  IInputProps,
+  useTheme,
+  Center
+} from 'native-base'
+import { Eye, EyeSlash } from 'phosphor-react-native'
+import { useState } from 'react'
+
+interface InputProps {
+  type: 'text' | 'password'
+  inputProps?: IInputProps
+}
+
+export function Input({ type, inputProps }: InputProps) {
+  const [viewPassword, setViewPassword] = useState(false)
+  const { colors } = useTheme()
+
+  return (
+    <InputNativeBase
+      bg="gray.700"
+      paddingLeft={4}
+      paddingRight={type === 'text' ? 4 : 2}
+      py={3}
+      borderWidth={0}
+      borderRadius="6px"
+      fontFamily="body"
+      fontSize="md"
+      color="gray.200"
+      placeholderTextColor="gray.400"
+      _focus={{
+        bg: 'gray.700',
+        borderWidth: 1,
+        borderColor: 'gray.300'
+      }}
+      type={viewPassword ? 'text' : 'password'}
+      InputRightElement={
+        type === 'password' ? (
+          <TouchableOpacity
+            onPress={() => setViewPassword(state => !state)}
+            style={{ marginRight: 16 }}
+          >
+            {viewPassword ? (
+              <EyeSlash size={20} color={colors.gray[300]} />
+            ) : (
+              <Eye size={20} color={colors.gray[300]} />
+            )}
+          </TouchableOpacity>
+        ) : (
+          <></>
+        )
+      }
+      {...inputProps}
+    />
+  )
+}
