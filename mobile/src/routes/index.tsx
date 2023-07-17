340cfa29a7f3acf69a1e8448cc2f34e17ba3b7e7
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-17 09:00:50 -0300

    feat: Add Rout App.

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { Box, useTheme } from 'native-base'
 
 import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
 
 export function Routes() {
   const { colors } = useTheme()
@@ -13,7 +14,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.600">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
