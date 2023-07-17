39aa0e14050705a07a32f0919ea45a0cb82f5b4e
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-17 17:56:25 -0300

    feat: Created Input Search Advertisements.

---
+++ mobile/src/components/InputSearchAdvertisements.tsx
@@ -0,0 +1,46 @@
+import { TouchableOpacity } from 'react-native'
+import { MagnifyingGlass, Sliders } from 'phosphor-react-native'
+
+import {
+  Divider,
+  HStack,
+  Input as InputNativeBase,
+  useTheme
+} from 'native-base'
+
+export function InputSearchAdvertisements() {
+  const { colors } = useTheme()
+
+  return (
+    <InputNativeBase
+      bg="gray.700"
+      paddingLeft={4}
+      paddingRight="0px"
+      py={3}
+      borderWidth={0}
+      borderRadius="6px"
+      fontFamily="body"
+      fontSize="md"
+      color="gray.200"
+      placeholderTextColor="gray.400"
+      _focus={{
+        bg: 'gray.700',
+        borderWidth: 1,
+        borderColor: 'gray.300'
+      }}
+      type="text"
+      InputRightElement={
+        <HStack space={3} marginLeft={3} marginRight={4} alignItems="center">
+          <TouchableOpacity>
+            <MagnifyingGlass weight="bold" size={20} color={colors.gray[200]} />
+          </TouchableOpacity>
+          <Divider bg="gray.400" w="1px" h="18px" orientation="vertical" />
+          <TouchableOpacity>
+            <Sliders weight="bold" size={20} color={colors.gray[200]} />
+          </TouchableOpacity>
+        </HStack>
+      }
+      placeholder="Buscar anúncio"
+    />
+  )
+}
