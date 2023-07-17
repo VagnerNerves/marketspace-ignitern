a20ae7f7d8439e12f6ebdf76638ccbb9aceec66c
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-17 13:14:30 -0300

    feat: Created component Card Info Advertisements.

---
+++ mobile/src/components/CardInfoAdvertisements.tsx
@@ -0,0 +1,57 @@
+import { TouchableOpacity } from 'react-native'
+
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import { Tag, ArrowRight } from 'phosphor-react-native'
+
+import { HStack, Text, VStack, useTheme } from 'native-base'
+
+interface CardInfoAdvertisementsProps {
+  numberTotalAdvertisements: number
+}
+
+export function CardInfoAdvertisements({
+  numberTotalAdvertisements
+}: CardInfoAdvertisementsProps) {
+  const { colors } = useTheme()
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleScreenMyAdvertisements() {
+    navigation.navigate('myAdvertisements')
+  }
+
+  return (
+    <TouchableOpacity onPress={handleScreenMyAdvertisements}>
+      <HStack
+        w="full"
+        py={3}
+        px={4}
+        backgroundColor="blue.100"
+        borderRadius="6px"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <HStack flex={1} space={4} alignItems="center">
+          <Tag weight="regular" size={22} color={colors.blue[700]} />
+
+          <VStack>
+            <Text fontFamily="heading" fontSize="xl" color="gray.200">
+              {numberTotalAdvertisements}
+            </Text>
+            <Text fontFamily="body" fontSize="xs" color="gray.200">
+              anúncions ativos
+            </Text>
+          </VStack>
+        </HStack>
+        <HStack alignItems="center" space={2}>
+          <Text fontFamily="heading" fontSize="xs" color="blue.700">
+            Meus anúncios
+          </Text>
+          <ArrowRight weight="bold" size={16} color={colors.blue[700]} />
+        </HStack>
+      </HStack>
+    </TouchableOpacity>
+  )
+}
