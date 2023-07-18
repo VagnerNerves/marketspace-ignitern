61b10aa090a4b58f87a1de924f2a28f8f3de468e
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-18 14:53:43 -0300

    feat: Finalized Layout.

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,83 @@
-import { Text, View } from 'react-native'
+import { useState } from 'react'
+import { FlatList, HStack, Text, VStack } from 'native-base'
+
+import { Button } from '@components/Button'
+import { UserPhoto } from '@components/UserPhoto'
+import { CardInfoAdvertisements } from '@components/CardInfoAdvertisements'
+import { InputSearchAdvertisements } from '@components/InputSearchAdvertisements'
+import { CardAdvertisements } from '@components/CardAdvertisements'
 
 export function Home() {
+  const [advertisement, setAdvertisement] = useState([
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6'
+  ])
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-    </View>
+    <VStack flex={1} paddingTop={16} px={6}>
+      <HStack
+        space={2}
+        alignItems="center"
+        justifyContent="space-between"
+        marginBottom={8}
+      >
+        <HStack flex={1} space={3} alignItems="center">
+          <UserPhoto
+            size={45}
+            borderWidth={2}
+            url="https://github.com/VagnerNerves.png"
+          />
+          <VStack flex={1}>
+            <Text fontFamily="body" fontSize="md" color="gray.100">
+              Boas vindas,
+            </Text>
+            <Text
+              fontFamily="heading"
+              fontSize="md"
+              color="gray.100"
+              numberOfLines={1}
+            >
+              Vagner Nerves!
+            </Text>
+          </VStack>
+        </HStack>
+        <Button
+          title="Criar anúncio"
+          typeColor="black"
+          typeIcon="plus"
+          buttonProps={{ width: 'auto' }}
+        />
+      </HStack>
+
+      <VStack space={3} marginBottom={8}>
+        <Text fontFamily="body" fontSize="sm" color="gray.300">
+          Seus produtos anunciados para venda
+        </Text>
+
+        <CardInfoAdvertisements numberTotalAdvertisements={4} />
+      </VStack>
+
+      <VStack space={3} marginBottom={6}>
+        <Text>Compre produtos variados</Text>
+
+        <InputSearchAdvertisements />
+      </VStack>
+
+      <FlatList
+        data={advertisement}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <CardAdvertisements />}
+        numColumns={2}
+        columnWrapperStyle={{
+          gap: 20,
+          paddingBottom: 24
+        }}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
   )
 }
