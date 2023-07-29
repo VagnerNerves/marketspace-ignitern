f65904b1edd26c2bd7a472c90c6f254a3a4962bd
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-28 22:09:37 -0300

    feat: Finalized layout this screen.

--- mobile/src/screens/DetailsAdvertisement.tsx
+++ mobile/src/screens/DetailsAdvertisement.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from 'react-native'
+import { VStack, Text, HStack } from 'native-base'
+
+import { HeaderNavigation } from '@components/HeaderNavigation'
+import { Button } from '@components/Button'
+import { ContentAdvertisement } from '@components/ContentAdvertisement'
 
 export function DetailsAdvertisement() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>DetailsAdvertisement</Text>
-    </View>
+    <VStack flex={1} paddingTop={16}>
+      <HeaderNavigation isGoBack hStackProps={{ px: 6, marginBottom: 3 }} />
+
+      <VStack flex={1}>
+        <ContentAdvertisement />
+      </VStack>
+
+      <HStack
+        bg="gray.700"
+        px={6}
+        paddingTop={5}
+        paddingBottom={7}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Text flex={1} fontFamily="heading" fontSize="2xl" color="blue.700">
+          <Text fontSize="sm">R$</Text> 120,00
+        </Text>
+        <Button
+          title="Entrar em contato"
+          typeColor="blue"
+          typeIcon="whatsapp"
+          buttonProps={{
+            flex: 1
+          }}
+        />
+      </HStack>
+    </VStack>
   )
 }
