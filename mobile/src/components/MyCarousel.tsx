fe9c35f181848ca52952aad01a80f31f744836fd
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-28 22:08:59 -0300

    feat: Created component MyCarousel.

---
+++ mobile/src/components/MyCarousel.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import { Dimensions } from 'react-native'
+import { HStack, Image, VStack } from 'native-base'
+
+import Carousel from 'react-native-reanimated-carousel'
+
+interface MyCarouselProps {
+  data: string[]
+  height?: number
+}
+export function MyCarousel({ data, height }: MyCarouselProps) {
+  const [currentPhoto, setCurrentPhoto] = useState<number>(0)
+
+  const width = Dimensions.get('window').width
+
+  if (data.length === 1) {
+    return (
+      <VStack>
+        <Image
+          source={{ uri: data[0] }}
+          alt="Image do produto"
+          resizeMode="cover"
+          width={width}
+          height={height ? height : width / 2}
+        />
+      </VStack>
+    )
+  }
+
+  return (
+    <VStack>
+      <Carousel
+        width={width}
+        height={height ? height : width / 2}
+        data={data}
+        pagingEnabled={true}
+        onSnapToItem={index => setCurrentPhoto(index)}
+        renderItem={({ index }) => (
+          <VStack
+            style={{
+              flex: 1
+            }}
+          >
+            <Image
+              source={{ uri: data[index] }}
+              alt="Image do produto"
+              resizeMode="cover"
+              flex={1}
+            />
+          </VStack>
+        )}
+      />
+      <HStack w="full" position="absolute" bottom="2px" px="2px" space={1}>
+        {data.map((value, index) => {
+          return (
+            <VStack
+              key={index}
+              h={1}
+              rounded="full"
+              flex={1}
+              backgroundColor={'gray.700'}
+              opacity={currentPhoto === index ? 100 : 50}
+            />
+          )
+        })}
+      </HStack>
+    </VStack>
+  )
+}
