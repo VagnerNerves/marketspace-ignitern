71a8495967c20baa5812a648e4eb90c1a7650555
Vagner Nerves <vagnernervessantos@gmail.com>
2023-08-21 19:00:29 -0300

    feat: Finalized MyAdvertisementes.

--- mobile/src/screens/MyAdvertisements.tsx
+++ mobile/src/screens/MyAdvertisements.tsx
@@ -1,9 +1,67 @@
-import { Text, View } from 'react-native'
+import { useState } from 'react'
+
+import { FlatList, HStack, Text, VStack } from 'native-base'
+
+import { HeaderNavigation } from '@components/HeaderNavigation'
+import { Select } from '@components/Select'
+import { CardAdvertisements } from '@components/CardAdvertisements'
 
 export function MyAdvertisements() {
+  const [service, setService] = useState('')
+
+  const [advertisement, setAdvertisement] = useState([
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6'
+  ])
+
+  console.log(service)
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>MyAdvertisements</Text>
-    </View>
+    <VStack px={6} paddingTop={16}>
+      <HeaderNavigation title="Meus anúncios" typeButton="plus" />
+
+      <HStack
+        justifyContent="space-between"
+        alignItems="center"
+        paddingTop={8}
+        paddingBottom={5}
+      >
+        <Text>9 anúncios</Text>
+
+        <Select
+          items={[
+            { label: 'Todos', value: 'all' },
+            { label: 'Ativos', value: 'active' },
+            { label: 'Inativos', value: 'inactive' }
+          ]}
+          selectProps={{
+            selectedValue: service,
+            onValueChange: item => setService(item)
+          }}
+        />
+      </HStack>
+
+      <FlatList
+        data={advertisement}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <CardAdvertisements
+            typeTag="used"
+            advertisements="inactive"
+            onNavigate={() => null}
+          />
+        )}
+        numColumns={2}
+        columnWrapperStyle={{
+          gap: 20,
+          paddingBottom: 24
+        }}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
   )
 }
