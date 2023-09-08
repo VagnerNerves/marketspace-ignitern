c74766d4b6a63c4c39c386ab1b2b51c79264b430
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-08 09:17:18 -0300

    feat: Created error message on input.

--- mobile/src/components/Input.tsx
+++ mobile/src/components/Input.tsx
@@ -3,7 +3,8 @@ import {
   Input as InputNativeBase,
   IInputProps,
   useTheme,
-  Text
+  Text,
+  FormControl
 } from 'native-base'
 import { Eye, EyeSlash } from 'phosphor-react-native'
 import { useState } from 'react'
@@ -11,63 +12,83 @@ import { useState } from 'react'
 interface InputProps {
   type: 'text' | 'password' | 'price'
   inputProps?: IInputProps
+  errorMessage?: string | null
 }
 
-export function Input({ type, inputProps }: InputProps) {
+export function Input({ type, inputProps, errorMessage = null }: InputProps) {
   const [viewPassword, setViewPassword] = useState(false)
+
+  const invalid = !!errorMessage || inputProps?.isInvalid
+
   const { colors } = useTheme()
 
   return (
-    <InputNativeBase
-      bg="gray.700"
-      paddingLeft={4}
-      paddingRight={type === 'text' ? 4 : 2}
-      py={3}
-      borderWidth={0}
-      borderRadius="6px"
-      fontFamily="body"
-      fontSize="md"
-      color="gray.200"
-      placeholderTextColor="gray.400"
-      _focus={{
-        bg: 'gray.700',
-        borderWidth: 1,
-        borderColor: 'gray.300'
-      }}
-      type={
-        type === 'text' || type === 'price'
-          ? 'text'
-          : viewPassword
-          ? 'text'
-          : 'password'
-      }
-      keyboardType={type === 'price' ? 'numeric' : 'default'}
-      InputLeftElement={
-        type === 'price' ? (
-          <Text fontFamily="body" fontSize="md" color="gray.100" marginLeft={4}>
-            R$
-          </Text>
-        ) : (
-          <></>
-        )
-      }
-      InputRightElement={
-        type === 'password' ? (
-          <TouchableOpacity
-            onPress={() => setViewPassword(state => !state)}
-            style={{ marginRight: 16 }}
-          >
-            {viewPassword ? (
-              <EyeSlash size={20} color={colors.gray[300]} />
-            ) : (
-              <Eye size={20} color={colors.gray[300]} />
-            )}
-          </TouchableOpacity>
-        ) : (
-          <></>
-        )
-      }
-      {...inputProps}
-    />
+    <FormControl isInvalid={invalid}>
+      <InputNativeBase
+        bg="gray.700"
+        paddingLeft={4}
+        paddingRight={type === 'text' ? 4 : 2}
+        py={3}
+        borderWidth={0}
+        borderRadius="6px"
+        fontFamily="body"
+        fontSize="md"
+        color="gray.200"
+        placeholderTextColor="gray.400"
+        isInvalid={invalid}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: 'error.500'
+        }}
+        _focus={{
+          bg: 'gray.700',
+          borderWidth: 1,
+          borderColor: 'gray.300'
+        }}
+        type={
+          type === 'text' || type === 'price'
+            ? 'text'
+            : viewPassword
+            ? 'text'
+            : 'password'
+        }
+        keyboardType={type === 'price' ? 'numeric' : 'default'}
+        InputLeftElement={
+          type === 'price' ? (
+            <Text
+              fontFamily="body"
+              fontSize="md"
+              color="gray.100"
+              marginLeft={4}
+            >
+              R$
+            </Text>
+          ) : (
+            <></>
+          )
+        }
+        InputRightElement={
+          type === 'password' ? (
+            <TouchableOpacity
+              onPress={() => setViewPassword(state => !state)}
+              style={{ marginRight: 16 }}
+            >
+              {viewPassword ? (
+                <EyeSlash size={20} color={colors.gray[300]} />
+              ) : (
+                <Eye size={20} color={colors.gray[300]} />
+              )}
+            </TouchableOpacity>
+          ) : (
+            <></>
+          )
+        }
+        {...inputProps}
+      />
+
+      <FormControl.ErrorMessage _text={{ color: 'error.600' }}>
+        {errorMessage}
+      </FormControl.ErrorMessage>
+    </FormControl>
   )
 }
