cf76f431e827b9142dea5b73f29a67338b5c87ae
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-17 12:38:42 -0300

    feat: Ajusted api with interceptors.

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,11 +2,11 @@ import axios from 'axios'
 import { AppError } from '@utils/AppError'
 
 const api = axios.create({
-  baseURL: '192.168.1.6:3333',
+  baseURL: 'http://192.168.1.6:3333',
   timeout: 6000
 })
 
-axios.interceptors.response.use(
+api.interceptors.response.use(
   response => response,
   error => {
     if (error.response && error.response.data) {
