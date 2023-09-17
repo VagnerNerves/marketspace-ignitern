4300f8ef2dc8ab55bf59223ecd77f69369ded93a
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-17 14:26:41 -0300

    feat: Ajusted user with type UserDTO and created loading and save storage.

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,8 +2,13 @@ import { ReactNode, createContext, useState } from 'react'
 
 import { api } from '@services/api'
 
+import { UserDTO } from '@dtos/UserDTO'
+import { storageUserSave } from '@storage/storageUser'
+import { storageAuthTokenSave } from '@storage/storageAuthToken'
+
 type AuthContextDataProps = {
-  user: any
+  user: UserDTO
+  isLoadingUser: boolean
   signIn: (email: string, password: string) => Promise<void>
 }
 
@@ -16,17 +21,42 @@ type AuthContextProviderProps = {
 }
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState<any>()
+  const [user, setUser] = useState<UserDTO>({} as UserDTO)
+  const [isLoadingUser, setIsLoadingUser] = useState(false)
+
+  async function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    setUser(userData)
+  }
+
+  async function storageUserAndTokenSave(
+    userData: UserDTO,
+    token: string,
+    refresh_token: string
+  ) {
+    try {
+      await storageUserSave(userData)
+      await storageAuthTokenSave({ token, refresh_token })
+    } catch (error) {
+      throw error
+    }
+  }
 
   async function signIn(email: string, password: string) {
     try {
+      setIsLoadingUser(true)
+
       const { data } = await api.post('/sessions', { email, password })
 
-      if (data.token && data.user && data['refresh-token']) {
-        console.log(data)
+      if (data.token && data.user && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token)
+
+        await userAndTokenUpdate(data.user, data.token)
       }
     } catch (error) {
       throw error
+    } finally {
+      setIsLoadingUser(false)
     }
   }
 
@@ -34,6 +64,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     <AuthContext.Provider
       value={{
         user,
+        isLoadingUser,
         signIn
       }}
     >
