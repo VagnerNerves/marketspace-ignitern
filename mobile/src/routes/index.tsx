8fc5de11630b888b8201d6a19729bcd2bc60c474
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-17 14:27:40 -0300

    feat: Ajusted for verify user and is Loading user.

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -2,19 +2,27 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 
 import { Box, useTheme } from 'native-base'
 
+import { useAuth } from '@hooks/useAuth'
+
 import { AuthRoutes } from './auth.routes'
 import { AppRoutes } from './app.routes'
+import { Loading } from '@components/Loading'
 
 export function Routes() {
   const { colors } = useTheme()
+  const { user, isLoadingUser } = useAuth()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[600]
 
+  if (isLoadingUser) {
+    return <Loading />
+  }
+
   return (
     <Box flex={1} bg="gray.600">
       <NavigationContainer theme={theme}>
-        <AppRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
