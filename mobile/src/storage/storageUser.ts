d538f35d27547d010cff32a8cdfaa01b7fa17136
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-17 13:53:23 -0300

    feat: Created functions  User save, get and remove.

---
+++ mobile/src/storage/storageUser.ts
@@ -0,0 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { USER_STORAGE } from './storageConfig'
+
+import { UserDTO } from '@dtos/UserDTO'
+
+export async function storageUserSave(user: UserDTO) {
+  await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user))
+}
+
+export async function storageUserGet() {
+  const storage = await AsyncStorage.getItem(USER_STORAGE)
+
+  const user: UserDTO = storage ? JSON.parse(storage) : {}
+
+  return user
+}
+
+export async function storageUserRemove() {
+  await AsyncStorage.removeItem(USER_STORAGE)
+}
