89dafa2b3376c686c4ab63eac6f26d1e89079e8a
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-17 08:28:23 -0300

    feat: Add React hook forma and validation with yup.

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,5 +1,12 @@
 import { Platform, TouchableOpacity } from 'react-native'
-import { User, PencilSimpleLine } from 'phosphor-react-native'
+
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+import * as ImagePicker from 'expo-image-picker'
+
+import { PencilSimpleLine } from 'phosphor-react-native'
 
 import {
   VStack,
@@ -7,7 +14,8 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   Heading,
-  useTheme
+  useTheme,
+  FormControl
 } from 'native-base'
 
 import { useNavigation } from '@react-navigation/native'
@@ -18,14 +26,73 @@ import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 import { UserPhoto } from '@components/UserPhoto'
+import { useState } from 'react'
+
+type FormDataProps = {
+  urlImage: string
+  name: string
+  email: string
+  tel: string
+  password: string
+  confirmPassword: string
+}
+
+const signUpSchema = yup.object({
+  urlImage: yup.string().required('Infome a foto'),
+  name: yup.string().required('Informe o nome.'),
+  email: yup.string().required('Informe o email.').email('Email Inválido.'),
+  tel: yup.string().required('Informe o telefone.'),
+  password: yup
+    .string()
+    .required('Informe a senha')
+    .min(6, 'A senha deve ter pelo menos 6 digítos.'),
+  confirmPassword: yup
+    .string()
+    .required('Informe a senha')
+    .min(6, 'A senha deve ter pelo menos 6 digítos.')
+})
 
 export function SignUp() {
+  const [imageSelect, setImageSelect] = useState<ImagePicker.ImagePickerAsset>(
+    {} as ImagePicker.ImagePickerAsset
+  )
   const { colors } = useTheme()
 
   const navigation = useNavigation<AuthNavigationRoutesProp>()
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  })
+
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  function handleSignUp(dataForm: FormDataProps) {
+    const { urlImage, name, email, tel, password } = dataForm
+
+    console.log(dataForm)
+  }
+
+  async function handleSearchImage() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1
+    })
+
+    if (!result.canceled) {
+      setImageSelect(result.assets[0])
+
+      return result.assets[0].uri
+    }
+
+    return ''
   }
 
   return (
@@ -61,38 +128,136 @@ export function SignUp() {
 
           <VStack flex={1} space={6}>
             <VStack space={4} alignItems="center">
-              <VStack width={88}>
-                <UserPhoto
-                  size={88}
-                  borderWidth={3}
-                  url="https://github.com/VagnerNerves.png"
-                />
-                <TouchableOpacity
-                  style={{ position: 'absolute', bottom: 0, right: -10 }}
-                >
-                  <VStack
-                    w={10}
-                    h={10}
-                    bg="blue.400"
-                    rounded="full"
+              <Controller
+                control={control}
+                name="urlImage"
+                render={({ field: { onChange, value } }) => (
+                  <FormControl
+                    isInvalid={!!errors.urlImage?.message}
                     alignItems="center"
-                    justifyContent="center"
                   >
-                    <PencilSimpleLine size={16} color={colors.gray[600]} />
-                  </VStack>
-                </TouchableOpacity>
-              </VStack>
+                    <VStack width={88}>
+                      <UserPhoto
+                        size={88}
+                        borderWidth={3}
+                        url={!!imageSelect.uri ? imageSelect.uri : undefined}
+                      />
+                      <TouchableOpacity
+                        style={{ position: 'absolute', bottom: 0, right: -10 }}
+                        onPress={async () => {
+                          const urlImage = await handleSearchImage()
+                          onChange(urlImage)
+                        }}
+                      >
+                        <VStack
+                          w={10}
+                          h={10}
+                          bg="blue.400"
+                          rounded="full"
+                          alignItems="center"
+                          justifyContent="center"
+                        >
+                          <PencilSimpleLine
+                            size={16}
+                            color={colors.gray[600]}
+                          />
+                        </VStack>
+                      </TouchableOpacity>
+                    </VStack>
+                    <FormControl.ErrorMessage _text={{ color: 'error.600' }}>
+                      {errors.urlImage?.message}
+                    </FormControl.ErrorMessage>
+                  </FormControl>
+                )}
+              />
 
-              <Input type="text" inputProps={{ placeholder: 'Nome' }} />
-              <Input type="text" inputProps={{ placeholder: 'E-mail' }} />
-              <Input type="text" inputProps={{ placeholder: 'Telefone' }} />
-              <Input type="password" inputProps={{ placeholder: 'Senha' }} />
-              <Input
-                type="password"
-                inputProps={{ placeholder: 'Confirmar senha' }}
+              <Controller
+                control={control}
+                name="name"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    type="text"
+                    inputProps={{
+                      placeholder: 'Nome',
+                      onChangeText: onChange,
+                      value: value
+                    }}
+                    errorMessage={errors.name?.message}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    type="text"
+                    inputProps={{
+                      placeholder: 'E-mail',
+                      onChangeText: onChange,
+                      value: value,
+                      keyboardType: 'email-address'
+                    }}
+                    errorMessage={errors.email?.message}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="tel"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    type="text"
+                    inputProps={{
+                      placeholder: 'Telefone',
+                      onChangeText: onChange,
+                      value: value,
+                      keyboardType: 'phone-pad'
+                    }}
+                    errorMessage={errors.tel?.message}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    type="password"
+                    inputProps={{
+                      placeholder: 'Senha',
+                      onChangeText: onChange,
+                      value: value
+                    }}
+                    errorMessage={errors.password?.message}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="confirmPassword"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    type="password"
+                    inputProps={{
+                      placeholder: 'Confirmar senha',
+                      onChangeText: onChange,
+                      value: value
+                    }}
+                    errorMessage={errors.confirmPassword?.message}
+                  />
+                )}
               />
             </VStack>
-            <Button title="Criar" typeColor="black" />
+            <Button
+              title="Criar"
+              typeColor="black"
+              buttonProps={{ onPress: handleSubmit(handleSignUp) }}
+            />
           </VStack>
 
           <VStack paddingY="46px" space={4}>
