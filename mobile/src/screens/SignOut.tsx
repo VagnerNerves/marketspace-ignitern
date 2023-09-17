776f4f81a35a3e6530b85563136de8e12e4eb383
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-17 18:20:57 -0300

    feat: Add Sign Out this screan.

--- mobile/src/screens/SignOut.tsx
+++ mobile/src/screens/SignOut.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from 'react'
+
 import { Loading } from '@components/Loading'
+import { useAuth } from '@hooks/useAuth'
 
 export function SignOut() {
+  const { singOut } = useAuth()
+
+  useEffect(() => {
+    singOut()
+  }, [])
+
   return <Loading />
 }
