64251d3326d8e2208af797ab79d154b6009cc7d7
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-17 18:53:52 -0300

    feat: Add Alert with options for loggout.

--- mobile/src/screens/SignOut.tsx
+++ mobile/src/screens/SignOut.tsx
@@ -1,14 +1,33 @@
-import { useEffect } from 'react'
+import { useCallback } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
 
 import { Loading } from '@components/Loading'
 import { useAuth } from '@hooks/useAuth'
+import { Alert } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { AppBottomNavigatorRoutesProps } from '@routes/app.routes'
 
 export function SignOut() {
   const { singOut } = useAuth()
+  const navigation = useNavigation<AppBottomNavigatorRoutesProps>()
 
-  useEffect(() => {
-    singOut()
-  }, [])
+  useFocusEffect(
+    useCallback(
+      () =>
+        Alert.alert('Deslogar', 'Deseja realmente sair da sua conta?', [
+          {
+            text: 'Sim',
+            onPress: () => singOut()
+          },
+          {
+            text: 'Não',
+            style: 'cancel',
+            onPress: () => navigation.navigate('home')
+          }
+        ]),
+      []
+    )
+  )
 
   return <Loading />
 }
