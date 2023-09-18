b50e414d806832a264e66119b7fdda96fab7081f
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-18 10:06:48 -0300

    feat: Created Product DTO

---
+++ mobile/src/dtos/ProductDTO.ts
@@ -0,0 +1,16 @@
+import { PaymentMetodDTO } from './PaymentMethodDTO'
+import { ProductImageDTO } from './ProductImageDTO'
+import { UserDTO } from './UserDTO'
+
+export type ProductDTO = {
+  id: string
+  name: string
+  description: string
+  price: number
+  is_new: boolean
+  accept_trade: boolean
+  is_active: boolean
+  product_images: ProductImageDTO[]
+  payment_metods: PaymentMetodDTO[]
+  user: UserDTO
+}
