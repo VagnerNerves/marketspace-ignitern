95efd82982a23cd735e976f98574b17ec7965de4
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-18 16:55:35 -0300

    feat: Add MytootProducst.

--- mobile/src/contexts/ProductContext.tsx
+++ mobile/src/contexts/ProductContext.tsx
@@ -4,8 +4,15 @@ import { api } from '@services/api'
 
 import { ProductDTO } from '@dtos/ProductDTO'
 
+type MyTotProductsProps = {
+  totProducts: number
+  totProductsIsActive: number
+  totProductsIsNotActive: number
+}
+
 type ProductContextDataProps = {
   myProducts: ProductDTO[]
+  myTotProducts: MyTotProductsProps
   getMyProducts: () => Promise<void>
 }
 
@@ -21,11 +28,27 @@ export function ProductContextProvider({
   children
 }: ProductContextProviderProps) {
   const [myProducts, setMyProducts] = useState<ProductDTO[]>([] as ProductDTO[])
+  const [myTotProducts, setMyTotProducts] = useState<MyTotProductsProps>(
+    {} as MyTotProductsProps
+  )
+
+  function totMyProducts(products: ProductDTO[]) {
+    setMyTotProducts({
+      totProducts: products.length,
+      totProductsIsActive: products.filter(
+        product => product.is_active === true
+      ).length,
+      totProductsIsNotActive: products.filter(
+        product => product.is_active === false
+      ).length
+    })
+  }
 
   async function getMyProducts() {
     try {
       const { data } = await api.get('/users/products')
 
+      totMyProducts(data)
       setMyProducts(data)
     } catch (error) {
       throw error
@@ -33,7 +56,9 @@ export function ProductContextProvider({
   }
 
   return (
-    <ProductContext.Provider value={{ myProducts, getMyProducts }}>
+    <ProductContext.Provider
+      value={{ myProducts, myTotProducts, getMyProducts }}
+    >
       {children}
     </ProductContext.Provider>
   )
