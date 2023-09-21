f95206e36ff8ab09e3331c2484363d0ad0272aed
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-21 10:23:34 -0300

    feat: Ajusted name and add paymentMethod the DB.

--- mobile/src/dtos/PaymentMethodDTO.ts
+++ mobile/src/dtos/PaymentMethodDTO.ts
@@ -1,4 +1,27 @@
-export type PaymentMetodDTO = {
+export type PaymentMethodDTO = {
   key: string
   name: string
 }
+
+export const paymentMethodsDB: PaymentMethodDTO[] = [
+  {
+    key: 'boleto',
+    name: 'Boleto'
+  },
+  {
+    key: 'pix',
+    name: 'Pix'
+  },
+  {
+    key: 'cash',
+    name: 'Dinheiro'
+  },
+  {
+    key: 'card',
+    name: 'Cartão de Crédito'
+  },
+  {
+    key: 'deposit',
+    name: 'Depósito Bancário'
+  }
+]
