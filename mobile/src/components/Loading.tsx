c2dca36200475184c7791658406d70c2b7fb482a
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-21 10:23:48 -0300

    feat: Add color blue.700

--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { Center, Spinner } from 'native-base'
 export function Loading() {
   return (
     <Center flex={1}>
-      <Spinner />
+      <Spinner color="blue.700" />
     </Center>
   )
 }
