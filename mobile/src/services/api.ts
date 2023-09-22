fd7621ecb46569b7029227bdac685a8ef4433bd9
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-22 08:26:52 -0300

    feat: Altered url.

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { AppError } from '@utils/AppError'
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.6:3333',
+  baseURL: 'http://192.168.1.4:3333',
   timeout: 6000
 })
 
