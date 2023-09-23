0263c0bab57d9d7d667bbe258a34725fc8674920
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-23 09:12:51 -0300

    feat: Ajusted for get product and used on screen.

--- mobile/src/screens/DetailsAdvertisement.tsx
+++ mobile/src/screens/DetailsAdvertisement.tsx
@@ -1,38 +1,109 @@
-import { VStack, Text, HStack } from 'native-base'
+import { useEffect, useState } from 'react'
+import { VStack, Text, HStack, useToast } from 'native-base'
+
+import { useNavigation, useRoute } from '@react-navigation/native'
+
+import { useProduct } from '@hooks/useProduct'
+
+import { AppError } from '@utils/AppError'
+import { formattedNumberForRealBRL } from '@utils/validationAndFormattedNumberForReal'
 
 import { HeaderNavigation } from '@components/HeaderNavigation'
 import { Button } from '@components/Button'
 import { ContentAdvertisement } from '@components/ContentAdvertisement'
+import { Loading } from '@components/Loading'
+import { Alert, Linking } from 'react-native'
+
+type RouteParamsProps = {
+  id: string
+}
 
 export function DetailsAdvertisement() {
+  const [isLoadingGetProduct, setIsLoadingGetProduct] = useState<boolean>(true)
+
+  const { product, getProduct } = useProduct()
+  const route = useRoute()
+  const navigate = useNavigation()
+  const toast = useToast()
+
+  const { id } = route.params as RouteParamsProps
+
+  async function handleGetInTouch() {
+    const message = `Estou entrando em contato pois fiquei interessado no produto ${product.name}. Ainda esta dísponivel?`
+
+    const urlWhats = `https://wa.me/55${product.user.tel}?text=${message}`
+
+    const supportedLink = await Linking.canOpenURL(urlWhats)
+
+    if (supportedLink) {
+      Linking.openURL(urlWhats)
+    } else {
+      Alert.alert(
+        'Contato',
+        `Favor entrar em contato com o anunciante pelo telefone: 55 ${product.user.tel}`
+      )
+    }
+  }
+
+  useEffect(() => {
+    try {
+      setIsLoadingGetProduct(true)
+
+      getProduct(id)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const messageError = isAppError
+        ? error.message
+        : 'Não foi possível buscar o anúncio. Tente novamente mais tarde'
+
+      toast.show({
+        title: messageError,
+        placement: 'top',
+        bgColor: 'error.600'
+      })
+
+      navigate.goBack()
+    } finally {
+      setIsLoadingGetProduct(false)
+    }
+  }, [])
+
   return (
     <VStack flex={1} paddingTop={16}>
       <HeaderNavigation isGoBack hStackProps={{ px: 6, marginBottom: 3 }} />
 
-      <VStack flex={1}>
-        <ContentAdvertisement />
-      </VStack>
+      {isLoadingGetProduct ? (
+        <Loading />
+      ) : (
+        <>
+          <VStack flex={1}>
+            <ContentAdvertisement product={product} />
+          </VStack>
 
-      <HStack
-        bg="gray.700"
-        px={6}
-        paddingTop={5}
-        paddingBottom={7}
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Text flex={1} fontFamily="heading" fontSize="2xl" color="blue.700">
-          <Text fontSize="sm">R$</Text> 120,00
-        </Text>
-        <Button
-          title="Entrar em contato"
-          typeColor="blue"
-          typeIcon="whatsapp"
-          buttonProps={{
-            flex: 1
-          }}
-        />
-      </HStack>
+          <HStack
+            bg="gray.700"
+            px={6}
+            paddingTop={5}
+            paddingBottom={7}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Text flex={1} fontFamily="heading" fontSize="2xl" color="blue.700">
+              <Text fontSize="sm">R$</Text>{' '}
+              {formattedNumberForRealBRL(product.price)}
+            </Text>
+            <Button
+              title="Entrar em contato"
+              typeColor="blue"
+              typeIcon="whatsapp"
+              buttonProps={{
+                flex: 1,
+                onPress: handleGetInTouch
+              }}
+            />
+          </HStack>
+        </>
+      )}
     </VStack>
   )
 }
--- mobile/src/screens/DetailsMyAdvertisement.tsx
+++ mobile/src/screens/DetailsMyAdvertisement.tsx
@@ -1,17 +1,105 @@
-import { useState } from 'react'
-import { VStack, Text, HStack, ScrollView } from 'native-base'
+import { useEffect, useState } from 'react'
+import { VStack, ScrollView, useToast } from 'native-base'
 
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { AppStackNavigatorRoutesProps } from '@routes/app.routes'
 
 import { HeaderNavigation } from '@components/HeaderNavigation'
 import { Button } from '@components/Button'
 import { ContentAdvertisement } from '@components/ContentAdvertisement'
+import { useProduct } from '@hooks/useProduct'
+import { AppError } from '@utils/AppError'
+import { Loading } from '@components/Loading'
+
+type RouteParamsProps = {
+  id: string
+}
 
 export function DetailsMyAdvertisement() {
-  const [activeAdvertisement, setActiveAdvertisement] = useState<boolean>(true)
+  const [isLoadingGetProduct, setIsLoadingGetProduct] = useState<boolean>(true)
+  const [isLoadingActiveOrInactive, setIsLoadingActiveOrInactive] =
+    useState<boolean>(false)
+  const [isLoadingDelete, setIsLoadingDelete] = useState<boolean>(false)
+
+  const {
+    product,
+    getProduct,
+    updateProductForActiveOrInactive,
+    deleteProduct
+  } = useProduct()
+  const route = useRoute()
+  const navigate = useNavigation()
+  const toast = useToast()
+
+  const { id } = route.params as RouteParamsProps
 
   const navigatorStack = useNavigation<AppStackNavigatorRoutesProps>()
+
+  async function handleActiveOrInactiveAdvertisement() {
+    try {
+      setIsLoadingActiveOrInactive(true)
+
+      await updateProductForActiveOrInactive(product.id, !product.is_active)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const messageError = isAppError
+        ? error.message
+        : 'Não foi possível desativa o anúncio no momento.'
+
+      toast.show({
+        title: messageError,
+        placement: 'top',
+        bgColor: 'error.600'
+      })
+    } finally {
+      setIsLoadingActiveOrInactive(false)
+    }
+  }
+
+  async function handleDeleteProduct() {
+    try {
+      setIsLoadingDelete(true)
+      await deleteProduct(product.id)
+
+      navigate.goBack()
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const messageError = isAppError
+        ? error.message
+        : 'Não foi possível deletar o anúncio. Tente novamente mais tarde.'
+
+      toast.show({
+        title: messageError,
+        placement: 'top',
+        bgColor: 'error.600'
+      })
+    } finally {
+      setIsLoadingDelete(false)
+    }
+  }
+
+  useEffect(() => {
+    try {
+      setIsLoadingGetProduct(true)
+
+      getProduct(id)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const messageError = isAppError
+        ? error.message
+        : 'Não foi possível buscar o anúncio. Tente novamente mais tarde'
+
+      toast.show({
+        title: messageError,
+        placement: 'top',
+        bgColor: 'error.600'
+      })
+
+      navigate.goBack()
+    } finally {
+      setIsLoadingGetProduct(false)
+    }
+  }, [])
 
   return (
     <VStack flex={1} paddingTop={16}>
@@ -22,32 +110,48 @@ export function DetailsMyAdvertisement() {
         hStackProps={{ px: 6, marginBottom: 3 }}
       />
 
-      <ScrollView flex={1}>
-        <ContentAdvertisement />
+      {isLoadingGetProduct ? (
+        <Loading />
+      ) : (
+        <ScrollView flex={1}>
+          <ContentAdvertisement product={product} />
 
-        <VStack
-          px={6}
-          paddingTop={5}
-          paddingBottom={7}
-          alignItems="center"
-          justifyContent="space-between"
-          space={2}
-        >
-          <Button
-            title={
-              activeAdvertisement === true
-                ? 'Desativar anúncio'
-                : 'Reativar anúncio'
-            }
-            typeColor={activeAdvertisement === true ? 'black' : 'blue'}
-            typeIcon="power"
-            buttonProps={{
-              onPress: () => setActiveAdvertisement(current => !current)
-            }}
-          />
-          <Button title="Excluir anúncio" typeColor="gray" typeIcon="trash" />
-        </VStack>
-      </ScrollView>
+          <VStack
+            px={6}
+            paddingTop={5}
+            paddingBottom={7}
+            alignItems="center"
+            justifyContent="space-between"
+            space={2}
+          >
+            <Button
+              title={
+                product.is_active === true
+                  ? 'Desativar anúncio'
+                  : 'Reativar anúncio'
+              }
+              typeColor={product.is_active === true ? 'black' : 'blue'}
+              typeIcon="power"
+              buttonProps={{
+                onPress: handleActiveOrInactiveAdvertisement,
+                isLoading: isLoadingActiveOrInactive,
+                isDisabled: isLoadingDelete
+              }}
+            />
+
+            <Button
+              title="Excluir anúncio"
+              typeColor="gray"
+              typeIcon="trash"
+              buttonProps={{
+                onPress: handleDeleteProduct,
+                isLoading: isLoadingDelete,
+                isDisabled: isLoadingActiveOrInactive
+              }}
+            />
+          </VStack>
+        </ScrollView>
+      )}
     </VStack>
   )
 }
