dd7ebd8b0cfeb9dc0e2124785d8bef0c256818a7
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-28 09:17:02 -0300

    feat: Add Message Error on componnent.

--- mobile/src/components/Radio.tsx
+++ mobile/src/components/Radio.tsx
@@ -1,4 +1,9 @@
-import { HStack, IRadioGroupProps, Radio as RadioNB } from 'native-base'
+import {
+  FormControl,
+  HStack,
+  IRadioGroupProps,
+  Radio as RadioNB
+} from 'native-base'
 
 interface RadioProps {
   radioGroupProps: IRadioGroupProps
@@ -6,31 +11,45 @@ interface RadioProps {
     value: string
     text: string
   }[]
+  errorMessage?: string | null
 }
 
-export function Radio({ radioGroupProps, options }: RadioProps) {
+export function Radio({
+  radioGroupProps,
+  options,
+  errorMessage = null
+}: RadioProps) {
+  const invalid = !!errorMessage
+
   return (
-    <RadioNB.Group {...radioGroupProps}>
-      <HStack space={5}>
-        {options.map(option => {
-          return (
-            <RadioNB
-              key={option.value}
-              value={option.value}
-              _checked={{
-                borderColor: 'blue.400',
-                _icon: { color: 'blue.400' }
-              }}
-              _pressed={{
-                borderColor: 'blue.700',
-                _icon: { color: 'blue.700' }
-              }}
-            >
-              {option.text}
-            </RadioNB>
-          )
-        })}
-      </HStack>
-    </RadioNB.Group>
+    <>
+      <RadioNB.Group {...radioGroupProps}>
+        <HStack space={5}>
+          {options.map(option => {
+            return (
+              <RadioNB
+                key={option.value}
+                value={option.value}
+                _checked={{
+                  borderColor: 'blue.400',
+                  _icon: { color: 'blue.400' }
+                }}
+                _pressed={{
+                  borderColor: 'blue.700',
+                  _icon: { color: 'blue.700' }
+                }}
+              >
+                {option.text}
+              </RadioNB>
+            )
+          })}
+        </HStack>
+      </RadioNB.Group>
+      <FormControl isInvalid={invalid}>
+        <FormControl.ErrorMessage _text={{ color: 'error.600' }}>
+          {errorMessage}
+        </FormControl.ErrorMessage>
+      </FormControl>
+    </>
   )
 }
